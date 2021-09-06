4d6e1c68c922b0ed017a59c8fc38cd6cc73f3134
CYee3589 <46963282+CYee3589@users.noreply.github.com>
2021-09-06 16:30:43 -0400

    Testing chars

--- foundationsProject/main.cpp
+++ foundationsProject/main.cpp
@@ -1,14 +1,19 @@
 //
-//  main.cpp
 //  foundationsProject
 //
 //  Created by Calvin Yee on 9/6/21.
-//
+//  For alphabets, I will use arrays
+//  For
 
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
+    char a = '1';
+    char b = 'd';
     std::cout << "Hello, World!\n";
+    if(a < b){
+        std::cout << "Hello, World!\n";
+    }
     return 0;
 }
