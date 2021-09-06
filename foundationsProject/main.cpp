7dab611e37bb5df639a519ef305d73b2bb2a0559
CYee3589 <46963282+CYee3589@users.noreply.github.com>
2021-09-06 16:13:54 -0400

    Initial commit

---
+++ foundationsProject/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  foundationsProject
+//
+//  Created by Calvin Yee on 9/6/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
